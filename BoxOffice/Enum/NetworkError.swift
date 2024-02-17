98da60b573c65e51fa757fcbe04fb6ce4d0dd45b
Hoi.Kwon <teaho177@gmail.com>
2024-02-17 17:50:40 +0900

    feat: 파일분류

---
+++ BoxOffice/Enum/NetworkError.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkError.swift
+//  BoxOffice
+//
+//  Created by 권태호 on 17/02/2024.
+//
+
+import Foundation
---
+++ BoxOffice/Model/MovieInfo.swift
@@ -0,0 +1,8 @@
+//
+//  MovieInfo.swift
+//  BoxOffice
+//
+//  Created by 권태호 on 17/02/2024.
+//
+
+import Foundation
